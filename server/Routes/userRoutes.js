da12e85e01055ce9a3bb9849c1dc342cd0e10424
Dhruv-Bhaskar <dhruvkumarbhaskar599@outlook.com>
2025-07-05 22:00:12 +0530

    added vercel.config

--- server/Routes/userRoutes.js
+++ server/Routes/userRoutes.js
@@ -53,8 +53,8 @@ router.post('/login', async (req,res)=>{
         const token = createToken(user._id)
         res.cookie('token', token,{
             httpOnly: true,
-            secure: false,
-            sameSite: 'lax',
+            secure: true,
+            sameSite: 'none',
             // maxAge: 7 * 24 * 60 * 60 * 1000
         })
 
