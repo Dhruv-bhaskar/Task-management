a3be123572d1b8839141b9e31128e4b6f9b30a43
Dhruv-bhaskar <dhruvkumarbhaskar599@outlook.com>
2025-08-24 17:42:08 +0530

    changed navbar bg

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -9,7 +9,7 @@ const Navbar = () => {
   };
 
   return (
-    <nav className="flex justify-between items-center w-full h-[5.1rem] rounded-md px-4 bg-black/5 mb-10 dark:bg-gray-400/70 transition-colors duration-300">
+    <nav className="flex justify-between items-center w-full h-[5.1rem] rounded-md px-4 bg-black/5 mb-10 dark:bg-black/45 transition-colors duration-300">
       <img
         src="/taskify.png"
         alt="logo"
